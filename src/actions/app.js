3b1317cc129708e973871e0b9b93780cbe4df45d
WojtekSzafraniec <feelun132@gmail.com>
2016-12-03 11:34:13 +0100

    actions example + constants

--- src/actions/app.js
+++ src/actions/app.js
@@ -3,7 +3,7 @@ import { fetch } from '../fetch';
 export const mockSuccess = (response) => ({ type: 'MOCK_SUCCESS', payload: response });
 export const mockError = (error) => ({ type: 'MOCK_FAILURE', payload: error });
 
-export const mockHTTPRequest = () => async (dispatch): void => {
+export const mockHTTPRequest = () => async (dispatch) => {
   try {
     const response = await fetch('https://httpbin.org/get', {
       method: 'GET',
---
+++ src/constants.js
@@ -0,0 +1,6 @@
+
+export const REGION = { identifier: 'Callstack', uuid: 'f7826da6-4fa2-4e98-8024-bc5b71e0893e' }; // Region
+
+export const MAJORIDS = [38488, 32301]; // beacon IDS
+
+export const APILINK = 'http://192.168.1.47:1880'; 
