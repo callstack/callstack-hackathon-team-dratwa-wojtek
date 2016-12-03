743da1146671bc7c79d8daae1e073cf9e0bd7aa8
Piotr Witek <piotrek.witek@gmail.com>
2016-12-03 14:33:01 +0100

    updated router config

--- src/router.js
+++ src/router.js
@@ -3,13 +3,10 @@ import React, { Component } from 'react';
 import {createRouter} from '@exponent/ex-navigation';
 import QRCodeContainer from './containers/QRCodeContainer';
 import HomeScreen from './containers/HomeScreen';
-import FirstTab from './containers/FirstTab';
 
 export const Router = createRouter(() => ({
-  tab: () => FirstTab,
-  home: () => ({
-    render: Router._makeRoute(QRCodeContainer),
-  })
+  home: () => HomeScreen,
+  tab: () => QRCodeContainer,
 }))
 
 export default Router;
