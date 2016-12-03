2cf0529acfb4a700275dfefc47faf33b46067b8e
WojtekSzafraniec <feelun132@gmail.com>
2016-12-03 11:22:38 +0100

    actions reducers setup

---
+++ src/actions/app.js
@@ -0,0 +1,19 @@
+/**
+* @flow
+*/
+
+import { fetch } from '../fetch';
+
+export const mockSuccess = (response) => ({ type: 'MOCK_SUCCESS', payload: response });
+export const mockError = (error) => ({ type: 'MOCK_FAILURE', payload: error });
+
+export const mockHTTPRequest = () => async (dispatch): void => {
+  try {
+    const response = await fetch('https://httpbin.org/get', {
+      method: 'GET',
+    });
+    dispatch(mockSuccess(response));
+  } catch (e) {
+    dispatch(mockError(e));
+  }
+};
