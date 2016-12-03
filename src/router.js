7010afebf1c29990d1e091beb3f52abc80f9ba0e
Alexandra Morozova <anarchistkasan@gmail.com>
2016-12-03 14:51:51 +0100

    Initial setup of getting started

--- src/router.js
+++ src/router.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 
 import {createRouter} from '@exponent/ex-navigation';
 import QRCodeContainer from './containers/QRCodeContainer';
+import GettingStarted from './containers/GettingStartedScreen';
 
 export const Router = createRouter(() => ({
   home: () => ({
-    render: Router._makeRoute(QRCodeContainer),
+    render: Router._makeRoute(GettingStarted)
+  }),
+  gettingStarted: () => ({
+    render: Router._makeRoute(GettingStarted)
   })
 }))
 
